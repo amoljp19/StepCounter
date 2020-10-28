307c5abdefbf1b1a747c232103ce463021f4b313
amol pawar <amoljp19@gmail.com>
2020-10-29 01:44:10 +0530

    added check permission and request permission code, also resolved buildconfig issue

--- app/src/main/java/com/softaai/simplestepcounter/MainActivity.kt
+++ app/src/main/java/com/softaai/simplestepcounter/MainActivity.kt
@@ -1,7 +1,18 @@
 package com.softaai.simplestepcounter
 
+import android.Manifest
+import android.content.Intent
+import android.content.pm.PackageManager
+import android.net.Uri
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.provider.Settings
+import android.util.Log
+import androidx.core.app.ActivityCompat
+import com.google.android.gms.auth.api.signin.GoogleSignIn
+import com.google.android.gms.fitness.FitnessOptions
+import com.google.android.gms.fitness.data.DataType
+import com.google.android.material.snackbar.Snackbar
 
 const val TAG = "SimpleStepCounter"
 
@@ -13,8 +24,122 @@ enum class FitActionRequestCode {
 
 class MainActivity : AppCompatActivity() {
 
+    private val fitnessOptions = FitnessOptions.builder()
+            .addDataType(DataType.TYPE_STEP_COUNT_CUMULATIVE)
+            .addDataType(DataType.TYPE_STEP_COUNT_DELTA)
+            .build()
+
+    private val runningQOrLater =
+            android.os.Build.VERSION.SDK_INT >= android.os.Build.VERSION_CODES.Q
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+    }
+
+    private fun checkPermissionsAndRun(fitActionRequestCode: FitActionRequestCode) {
+        if (permissionApproved()) {
+            fitSignIn(fitActionRequestCode)
+        } else {
+            requestRuntimePermissions(fitActionRequestCode)
+        }
+    }
+
+    private fun fitSignIn(requestCode: FitActionRequestCode) {
+        if (oAuthPermissionsApproved()) {
+            performActionForRequestCode(requestCode)
+        } else {
+            requestCode.let {
+                GoogleSignIn.requestPermissions(
+                        this,
+                        requestCode.ordinal,
+                        getGoogleAccount(), fitnessOptions)
+            }
+        }
+    }
+
+    private fun oAuthPermissionsApproved() = GoogleSignIn.hasPermissions(getGoogleAccount(), fitnessOptions)
+
+    private fun performActionForRequestCode(requestCode: FitActionRequestCode) = when (requestCode) {
+        FitActionRequestCode.READ_DATA -> readData()
+        FitActionRequestCode.SUBSCRIBE -> subscribe()
+    }
+
+    private fun subscribe() {
+
+    }
+
+    private fun readData(){
+
+    }
+
+    private fun getGoogleAccount() = GoogleSignIn.getAccountForExtension(this, fitnessOptions)
+
+    private fun permissionApproved(): Boolean {
+        val approved = if (runningQOrLater) {
+            PackageManager.PERMISSION_GRANTED == ActivityCompat.checkSelfPermission(
+                    this,
+                    Manifest.permission.ACTIVITY_RECOGNITION)
+        } else {
+            true
+        }
+        return approved
+    }
+
+    private fun requestRuntimePermissions(requestCode: FitActionRequestCode) {
+        val shouldProvideRationale =
+                ActivityCompat.shouldShowRequestPermissionRationale(this, Manifest.permission.ACTIVITY_RECOGNITION)
+
+        requestCode.let {
+            if (shouldProvideRationale) {
+                Log.i(TAG, "Displaying permission rationale to provide additional context.")
+                Snackbar.make(
+                        findViewById(R.id.main_activity_view),
+                        R.string.permission_rationale,
+                        Snackbar.LENGTH_INDEFINITE)
+                        .setAction(R.string.ok) {
+                            ActivityCompat.requestPermissions(this,
+                                    arrayOf(Manifest.permission.ACTIVITY_RECOGNITION),
+                                    requestCode.ordinal)
+                        }
+                        .show()
+            } else {
+                Log.i(TAG, "Requesting permission")
+                ActivityCompat.requestPermissions(this,
+                        arrayOf(Manifest.permission.ACTIVITY_RECOGNITION),
+                        requestCode.ordinal)
+            }
+        }
+    }
+
+    override fun onRequestPermissionsResult(requestCode: Int, permissions: Array<String>,
+                                            grantResults: IntArray) {
+        when {
+            grantResults.isEmpty() -> {
+                Log.i(TAG, "User interaction was cancelled.")
+            }
+            grantResults[0] == PackageManager.PERMISSION_GRANTED -> {
+                val fitActionRequestCode = FitActionRequestCode.values()[requestCode]
+                fitActionRequestCode.let {
+                    fitSignIn(fitActionRequestCode)
+                }
+            }
+            else -> {
+                Snackbar.make(
+                        findViewById(R.id.main_activity_view),
+                        R.string.permission_denied_explanation,
+                        Snackbar.LENGTH_INDEFINITE)
+                        .setAction(R.string.settings) {
+                            val intent = Intent()
+                            intent.action = Settings.ACTION_APPLICATION_DETAILS_SETTINGS
+                            val uri = Uri.fromParts("package",
+                                    BuildConfig.APPLICATION_ID, null)
+                            intent.data = uri
+                            intent.flags = Intent.FLAG_ACTIVITY_NEW_TASK
+                            startActivity(intent)
+                        }
+                        .show()
+            }
+        }
     }
 }
