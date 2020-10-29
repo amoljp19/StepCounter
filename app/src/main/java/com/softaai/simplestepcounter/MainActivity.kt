c74b15b8192e47299d9480b9df959edad40b8008
amol pawar <amoljp19@gmail.com>
2020-10-29 18:29:29 +0530

    showing steps data on chart

--- app/src/main/java/com/softaai/simplestepcounter/MainActivity.kt
+++ app/src/main/java/com/softaai/simplestepcounter/MainActivity.kt
@@ -5,31 +5,50 @@ import android.content.Intent
 import android.content.pm.PackageManager
 import android.net.Uri
 import android.os.Build
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.provider.Settings
 import android.util.Log
 import android.view.Menu
 import android.view.MenuItem
+import android.view.View
 import androidx.annotation.RequiresApi
+import androidx.appcompat.app.AppCompatActivity
 import androidx.core.app.ActivityCompat
+import com.github.mikephil.charting.charts.BarChart
+import com.github.mikephil.charting.components.Description
+import com.github.mikephil.charting.data.BarData
+import com.github.mikephil.charting.data.BarDataSet
+import com.github.mikephil.charting.data.BarEntry
+import com.github.mikephil.charting.interfaces.datasets.IBarDataSet
 import com.google.android.gms.auth.api.signin.GoogleSignIn
 import com.google.android.gms.fitness.Fitness
 import com.google.android.gms.fitness.FitnessOptions
+import com.google.android.gms.fitness.data.DataSet
 import com.google.android.gms.fitness.data.DataType
 import com.google.android.gms.fitness.data.Field
+import com.google.android.gms.fitness.request.DataReadRequest
 import com.google.android.material.snackbar.Snackbar
 import kotlinx.android.synthetic.main.activity_main.*
+import java.text.DateFormat
+import java.time.LocalDate
+import java.util.*
+import java.util.concurrent.TimeUnit
+import kotlin.collections.ArrayList
+import kotlin.math.absoluteValue
+
 
 const val TAG = "SimpleStepCounter"
 
 enum class FitActionRequestCode {
     SUBSCRIBE,
-    READ_DATA
+    READ_DATA,
+    PAST_14_DAYS_DATA
 }
 
 
 class MainActivity : AppCompatActivity() {
+
+    val PAST_14_DAYS = 14
 
     private val fitnessOptions = FitnessOptions.builder()
             .addDataType(DataType.TYPE_STEP_COUNT_CUMULATIVE)
@@ -39,12 +58,15 @@ class MainActivity : AppCompatActivity() {
     private val runningQOrLater =
             android.os.Build.VERSION.SDK_INT >= android.os.Build.VERSION_CODES.Q
 
+    private lateinit var dataSets: ArrayList<BarDataSet>
+
     @RequiresApi(Build.VERSION_CODES.Q)
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
         checkPermissionsAndRun(FitActionRequestCode.SUBSCRIBE)
+        //displayLastTwoWeeksData()
     }
 
     override fun onResume() {
@@ -69,7 +91,8 @@ class MainActivity : AppCompatActivity() {
                 GoogleSignIn.requestPermissions(
                         this,
                         requestCode.ordinal,
-                        getGoogleAccount(), fitnessOptions)
+                        getGoogleAccount(), fitnessOptions
+                )
             }
         }
     }
@@ -88,11 +111,15 @@ class MainActivity : AppCompatActivity() {
         }
     }
 
-    private fun oAuthPermissionsApproved() = GoogleSignIn.hasPermissions(getGoogleAccount(), fitnessOptions)
+    private fun oAuthPermissionsApproved() = GoogleSignIn.hasPermissions(
+            getGoogleAccount(),
+            fitnessOptions
+    )
 
     private fun performActionForRequestCode(requestCode: FitActionRequestCode) = when (requestCode) {
         FitActionRequestCode.READ_DATA -> readData()
         FitActionRequestCode.SUBSCRIBE -> subscribe()
+        FitActionRequestCode.PAST_14_DAYS_DATA -> displayLastTwoWeeksData()
     }
 
     private fun oAuthErrorMsg(requestCode: Int, resultCode: Int) {
@@ -144,6 +171,10 @@ class MainActivity : AppCompatActivity() {
             fitSignIn(FitActionRequestCode.READ_DATA)
             return true
         }
+        if (id == R.id.action_past_14_days_data) {
+            fitSignIn(FitActionRequestCode.PAST_14_DAYS_DATA)
+            return true
+        }
         return super.onOptionsItemSelected(item)
     }
 
@@ -154,7 +185,8 @@ class MainActivity : AppCompatActivity() {
         val approved = if (runningQOrLater) {
             PackageManager.PERMISSION_GRANTED == ActivityCompat.checkSelfPermission(
                     this,
-                    Manifest.permission.ACTIVITY_RECOGNITION)
+                    Manifest.permission.ACTIVITY_RECOGNITION
+            )
         } else {
             true
         }
@@ -164,7 +196,10 @@ class MainActivity : AppCompatActivity() {
     @RequiresApi(Build.VERSION_CODES.Q)
     private fun requestRuntimePermissions(requestCode: FitActionRequestCode) {
         val shouldProvideRationale =
-                ActivityCompat.shouldShowRequestPermissionRationale(this, Manifest.permission.ACTIVITY_RECOGNITION)
+                ActivityCompat.shouldShowRequestPermissionRationale(
+                        this,
+                        Manifest.permission.ACTIVITY_RECOGNITION
+                )
 
         requestCode.let {
             if (shouldProvideRationale) {
@@ -172,24 +207,31 @@ class MainActivity : AppCompatActivity() {
                 Snackbar.make(
                         findViewById(R.id.main_activity_view),
                         R.string.permission_rationale,
-                        Snackbar.LENGTH_INDEFINITE)
+                        Snackbar.LENGTH_INDEFINITE
+                )
                         .setAction(R.string.ok) {
-                            ActivityCompat.requestPermissions(this,
+                            ActivityCompat.requestPermissions(
+                                    this,
                                     arrayOf(Manifest.permission.ACTIVITY_RECOGNITION),
-                                    requestCode.ordinal)
+                                    requestCode.ordinal
+                            )
                         }
                         .show()
             } else {
                 Log.i(TAG, "Requesting permission")
-                ActivityCompat.requestPermissions(this,
+                ActivityCompat.requestPermissions(
+                        this,
                         arrayOf(Manifest.permission.ACTIVITY_RECOGNITION),
-                        requestCode.ordinal)
+                        requestCode.ordinal
+                )
             }
         }
     }
 
-    override fun onRequestPermissionsResult(requestCode: Int, permissions: Array<String>,
-                                            grantResults: IntArray) {
+    override fun onRequestPermissionsResult(
+            requestCode: Int, permissions: Array<String>,
+            grantResults: IntArray
+    ) {
         when {
             grantResults.isEmpty() -> {
                 Log.i(TAG, "User interaction was cancelled.")
@@ -204,12 +246,15 @@ class MainActivity : AppCompatActivity() {
                 Snackbar.make(
                         findViewById(R.id.main_activity_view),
                         R.string.permission_denied_explanation,
-                        Snackbar.LENGTH_INDEFINITE)
+                        Snackbar.LENGTH_INDEFINITE
+                )
                         .setAction(R.string.settings) {
                             val intent = Intent()
                             intent.action = Settings.ACTION_APPLICATION_DETAILS_SETTINGS
-                            val uri = Uri.fromParts("package",
-                                    BuildConfig.APPLICATION_ID, null)
+                            val uri = Uri.fromParts(
+                                    "package",
+                                    BuildConfig.APPLICATION_ID, null
+                            )
                             intent.data = uri
                             intent.flags = Intent.FLAG_ACTIVITY_NEW_TASK
                             startActivity(intent)
@@ -218,4 +263,121 @@ class MainActivity : AppCompatActivity() {
             }
         }
     }
+
+    private fun displayLastTwoWeeksData(){
+
+        val cal: Calendar = Calendar.getInstance()
+        val now = Date()
+        cal.setTime(now)
+        val endTime: Long = cal.timeInMillis
+        cal.add(Calendar.DATE, -14)
+        val startTime: Long = cal.timeInMillis
+
+        val dateFormat: DateFormat = DateFormat.getDateInstance()
+        val timeFormat = DateFormat.getTimeInstance()
+        Log.e("History", "Range Start: " + dateFormat.format(startTime))
+        Log.e("History", "Range End: " + dateFormat.format(endTime))
+
+        val readRequest = DataReadRequest.Builder()
+            .aggregate(DataType.TYPE_STEP_COUNT_DELTA, DataType.AGGREGATE_STEP_COUNT_DELTA)
+            .bucketByTime(1, TimeUnit.DAYS)
+            .setTimeRange(startTime, endTime, TimeUnit.MILLISECONDS)
+            .build()
+
+            Fitness.getHistoryClient(this, getGoogleAccount())
+                .readData(readRequest)
+                .addOnSuccessListener {
+                    it.buckets
+
+                    if (it.buckets.size > 0) {
+                        Log.e("History", "Number of buckets: " + it.buckets.size)
+                        for (bucket in it.buckets) {
+                            val dataSets: List<DataSet> = bucket.getDataSets()
+                            for (dataSet in dataSets) {
+                                showDataSet(dataSet, dateFormat, timeFormat)
+                            }
+                        }
+                    }
+                }
+                .addOnFailureListener { e ->
+                    Log.w(TAG, "There was a problem getting the step count.", e)
+                }
+
+
+    }
+
+    
+    private fun showDataSet(dataSet: DataSet, dateFormat: DateFormat, timeFormat: DateFormat){
+        var barDataSet:BarDataSet? = null
+        var dataSets = arrayListOf<BarDataSet>()
+        for (dp in dataSet.getDataPoints()) {
+            var valueSet = arrayListOf<BarEntry>();
+            Log.e("History", "Data point:")
+            Log.e("History", "\tType: " + dp.getDataType().getName())
+            Log.e(
+                    "History",
+                    "\tStart: " + dateFormat.format(dp.getStartTime(TimeUnit.MILLISECONDS))
+                            .toString() + " " + timeFormat.format(
+                            dp.getStartTime(
+                                    TimeUnit.MILLISECONDS
+                            )
+                    )
+            )
+            Log.e(
+                    "History",
+                    "\tEnd: " + dateFormat.format(dp.getEndTime(TimeUnit.MILLISECONDS))
+                            .toString() + " " + timeFormat.format(
+                            dp.getStartTime(
+                                    TimeUnit.MILLISECONDS
+                            )
+                    )
+            )
+
+            var day =dateFormat.format(dp.getEndTime(TimeUnit.MILLISECONDS)).toString().split("-")[0];
+            Log.e(
+                    "History",
+                    "\tday: " + day
+            )
+            for (field in dp.getDataType().getFields()) {
+                Log.e(
+                        "History", "\tField: " + field.name +
+                        " Value: " + dp.getValue(field)
+                )
+
+                val v = BarEntry(day.toFloat(), dp.getValue(field).asInt().toFloat())
+                valueSet.add(v)
+            }
+            barDataSet = BarDataSet(valueSet, dataSet.dataType.name)
+        }
+
+        val chart: BarChart = findViewById<View>(R.id.chart) as BarChart
+
+        val data = BarData()
+        barDataSet.let {
+            data.addDataSet(barDataSet)
+            chart.setData(data)
+            val description = Description()
+            description.text = "Last 14 days Steps Chart"
+            chart.description = description
+            chart.animateXY(2000, 2000)
+            chart.invalidate()
+        }
+
+
+    }
+
+    @Suppress("NULLABILITY_MISMATCH_BASED_ON_JAVA_ANNOTATIONS")
+    private fun getXAxisValues(): ArrayList<Any> {
+        val cal: Calendar = Calendar.getInstance()
+        val now = Date()
+        cal.setTime(now)
+        val xAxis = ArrayList<Any>()
+        for(i:Int in 14 downTo 1){
+            cal.add(Calendar.DATE, -i)
+            val day:String = cal.getDisplayName(Calendar.DAY_OF_WEEK, Calendar.LONG, Locale.ENGLISH)
+            xAxis.add(day)
+        }
+        return xAxis
+    }
+
 }
